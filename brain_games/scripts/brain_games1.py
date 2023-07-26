75983a664e779058db21157c80c4eebc6c4fb8e3
Evgenia Pavlenko <mozguru@mail.ru>
2023-07-26 10:14:59 +0300

    modified brain_games1.py

---
+++ brain_games/scripts/brain_games1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print('Welcome to the Brain Games!')
+
+def main():
+    if __name__ == '__main__':
+        main()
+
+from brain_games.cli import welcome_user
+welcome_user(__name__)

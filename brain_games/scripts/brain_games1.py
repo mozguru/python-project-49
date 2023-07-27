025ccb682e98c1f847c7ea21329ec3579fba6a73
Evgenia Pavlenko <mozguru@mail.ru>
2023-07-27 09:24:15 +0300

    linter modified brain_games1.py

--- brain_games/scripts/brain_games1.py
+++ brain_games/scripts/brain_games1.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+from brain_games.cli import welcome_user
+
+
 print('Welcome to the Brain Games!')
+
 
 def main():
     if __name__ == '__main__':
         main()
 
-from brain_games.cli import welcome_user
+
 welcome_user(__name__)

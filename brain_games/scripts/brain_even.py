1b73933143957602d364db6bc0891f6e138a7383
Evgenia Pavlenko <mozguru@mail.ru>
2023-08-04 16:40:28 +0300

    modified brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python3
 
+def main():
+    if __name__ == '__main__':
+        main()
+
 from random import randint
 
-print('Welcome to the Brain Games!')
-print('May I have your name?')
-name = input()
-print(f"Hello, {name}!")
-print('Answer "yes" if the number is even, otherwise answer "no".')
 
+def brain_even_game():
+    print('Welcome to the Brain Games!')
+    print('May I have your name?')
+    name = input()
+    print(f"Hello, {name}!")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
-def is_even():
-    number = randint(1, 100)
-    print('Question: ', number)
-    your_answer = input('Your answer: ')
-    if number % 2 == 0:
-        true_answer = 'yes'
-    else:
-        true_answer = 'no'
-    if your_answer != true_answer:
-        print(f"'{your_answer}' is wrong answer ;(. Correct answer was '{true_answer}'.")
-        print("Let's try again!")
-    else:
-        print('Correct!')
-
-
-is_even()
-
-
-def try_count():
     counter = 0
-    while counter < 2:
-        is_even()
-        counter += 1
+    while counter < 3:
+        number = randint(1, 100)
+        print('Question: ', number)
+        your_answer = input('Your answer: ')
+
+        if number % 2 == 0:
+            true_answer = 'yes'
+        else:
+            true_answer = 'no'
+
+        if your_answer == true_answer:
+            print('Correct!')
+            counter += 1
+
+        else:
+            print(f"'{your_answer}' is wrong answer ;(. Correct answer was '{true_answer}'.")
+            print("Let's try again!")
+            break
+
+    if counter == 3:
+        print(f"Congratulations, {name}!")
 
 
-try_count()
+brain_even_game()

caf99b7374467f9a200b6c7ee916a7c4612f2fc7
Evgenia Pavlenko <mozguru@mail.ru>
2023-08-10 10:48:48 +0300

    added brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from brain_games.games.game_logic import game_logic
+from brain_games.games import calc
+
+def main():
+    game_logic(calc)
+
+    if __name__ == '__main__':
+        main()

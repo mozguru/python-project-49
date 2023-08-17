fa73f86eb608725bfbde30a6c102d8b038d389c0
Evgenia Pavlenko <mozguru@mail.ru>
2023-08-17 17:50:56 +0300

    fixed profression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,18 +3,21 @@ from random import randint, choice
 GAME_QUESTION = 'What number is missing in the progression?'
 
 
-def brain_progression(start, stop, step):
-    length = 10
-    progression_list = list(range(start, stop, step))
-    return progression_list[:length]
+def brain_progression(start, step, length):
+    progression_list = []
+    i = 0
+    while i < length:
+        progression_list.append(start + step * i)
+        i += 1
+    return progression_list
 
 
 def game_start():
-    start_number = randint(1, 50)
-    stop_number = randint(50, 100)
-    step_number = randint(1, 5)
-    progression = brain_progression(start_number, stop_number, step_number)
+    start_number = randint(0, 100)
+    step_number = randint(1, 10)
+    length_number = randint(5, 10)
+    progression = brain_progression(start_number, step_number, length_number)
     progression_string = ' '.join(str(n) for n in progression)
-    correct_answer = choice(progression)
-    question = progression_string.replace(str(correct_answer), '..')
-    return question, str(correct_answer)
+    true_answer = choice(progression)
+    question = progression_string.replace(str(true_answer), '..', 1)
+    return question, str(true_answer)

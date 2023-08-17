09f1df0a5995bc38ce4c3494db044f16a8086c19
Evgenia Pavlenko <mozguru@mail.ru>
2023-08-17 13:16:06 +0300

    added prime.py

---
+++ brain_games/games/prime.py
@@ -0,0 +1,19 @@
+from random import randint
+
+GAME_QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def brain_prime(number):
+    if number > 1:
+        for i in range(2, int(number/2) + 1):
+            if (number % i) == 0:
+                return False
+        else:
+            return True
+
+
+def game_start():
+    number = randint(1, 100)
+    question = f'{number}'
+    true_answer = 'yes' if brain_prime(number) else 'no'
+    return question, true_answer

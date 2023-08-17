249877f9fa265c9d244b71099615cb9b484ad608
Evgenia Pavlenko <mozguru@mail.ru>
2023-08-17 15:59:53 +0300

    added even.py

---
+++ brain_games/games/even.py
@@ -0,0 +1,15 @@
+from random import randint
+
+GAME_QUESTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def brain_even(number):
+    if number % 2 == 0:
+        return number
+    
+
+def game_start():
+    number = randint(1, 100)
+    question = number
+    true_answer = 'yes' if brain_even(number) else 'no'
+    return str(question), true_answer
